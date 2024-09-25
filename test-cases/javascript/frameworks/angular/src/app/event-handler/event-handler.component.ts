8ace9ce2523a316b9d76842c39f5ace7c9296d59
Security Automation Team <security-crawl-maze@google.com>
2024-09-25 01:33:24 -0700

    No public description
    PiperOrigin-RevId: 678589170
    Change-Id: Iba94138ae83756103f11b3652fdd625ac53ec4c7

--- test-cases/javascript/frameworks/angular/src/app/event-handler/event-handler.component.ts
+++ test-cases/javascript/frameworks/angular/src/app/event-handler/event-handler.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'event-handler-navigation',
   template: `
     <h1>Event Handler Navigation</h1>
--- test-cases/javascript/frameworks/angular/src/app/router-outlet/router-outlet.component.ts
+++ test-cases/javascript/frameworks/angular/src/app/router-outlet/router-outlet.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'router-outlet-navigation',
   template: `
     <h1>Router Outlet Navigation</h1>
