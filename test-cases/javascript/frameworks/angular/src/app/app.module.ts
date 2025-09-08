0fbd9e1f877ded4cb5801c8a127dcc04c2a99784
Security Automation Team <security-crawl-maze@google.com>
2025-09-08 12:29:55 -0700

    No public description
    PiperOrigin-RevId: 804542241
    Change-Id: Ie0f7b5b48b755742a20cde3d50e6f40bf80d5baa

--- test-cases/javascript/frameworks/angular/src/app/app.module.ts
+++ test-cases/javascript/frameworks/angular/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import {NgModule} from '@angular/core';
+import {NgModule, provideZoneChangeDetection} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
+@NgModule({providers: [provideZoneChangeDetection()]})
+export class ZoneChangeDetectionModule {}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [ZoneChangeDetectionModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
